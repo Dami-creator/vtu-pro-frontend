4b6413a4fc4df7c6c2311af4d50cda6527a3df7b
Dami-creator <mcallendiana@gmail.com>
2026-02-23 03:07:41 +0100

    Update API base URL in app.js

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,6 +1,6 @@
 // Configuration
 const CONFIG = {
-    API_BASE_URL: 'https://vtu-pro-api.onrender.com/api',
+    API_BASE_URL: 'https://vtu-pro-frontend.onrender.com/',
  // Change to your backend URL
     PAYSTACK_PUBLIC_KEY: 'pk_live_b5622bad3a685b2e2d781934f3f1e84146dae9fe'
 };
